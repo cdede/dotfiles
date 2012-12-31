1673c945b34cbe29c3e9adad82798674e0ae39ea
noname <samqhrobya@liquidid.net>
2012-12-31 11:50:23 +0900

    home 2, var big

--- dot/.config/dupl/auto.sh
+++ dot/.config/dupl/auto.sh
@@ -1,13 +1,10 @@
 #!/bin/dash
-
 path2=$XDG_CONFIG_HOME/dupl
-path1=~/.config/priv1
-xset -dpms
 xrdb -m $path2/Xresources 
-setxkbmap -option terminate:ctrl_alt_bksp
 xmodmap $path2/xmodmaprc
+xset -dpms
 xset b off
 xset s off
 ! pgrep xbindkeys && xbindkeys -fg $path2/xbind.scm  &
-fcitx -d
-feh --bg-scale $path1/background.png
+dex -a 
+eval $(cat ~/.fehbg)
