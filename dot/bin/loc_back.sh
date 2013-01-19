78dae51868fb6e68c80f62d62b0d184c34b57aba
noname <samqhrobya@liquidid.net>
2013-01-19 20:13:59 +0900

    use eval

--- dot/bin/loc_back.sh
+++ dot/bin/loc_back.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 cmp_or_mv(){
-  cmp -s $1 $2 
+ #  $1 filename
+ #  $2 cmd
+  f2=$1
+  f1=$1.new
+  shift
+  cmd=$*
+  cmd1=$cmd">$f1"
+  eval $cmd1
+  cmp -s $f1 $f2
   if [ $? -eq 0  ] ;then
-    rm $1
+    rm $f1
   else
-    mv $1 $2
+    mv $f1 $f2
   fi
 
 }
-path1=~/.config/priv1
+pushd ~/.config/priv1
 set -- $(getopt s:lvmb "$@" )
 while [ $# -gt 0 ]
 do
     case "$1" in
     (-l) 
-      f1=`mktemp`
-      cd $path1
-      pacman -Qqe | grep -vx "$(pacman -Qqm)">$f1
-      cmp_or_mv $f1 pkg_`date "+%Y"`.list
-      pacman -Qq >$f1
-      cmp_or_mv $f1 pkg_aur_`date "+%Y"`.list
-      pacman -Qqm >$f1
-      cmp_or_mv $f1 aur_`date "+%Y"`.list
+      f1=pkg_`date "+%Y"`.list
+      c1='pacman -Qqe | grep -vx "$(pacman -Qqm)"'
+      cmp_or_mv $f1 $c1
+      c1='pacman -Qq'
+      f1=pkg_aur_`date "+%Y"`.list
+      cmp_or_mv $f1 $c1
+      f1=aur_`date "+%Y"`.list
+      c1='pacman -Qqm' 
+      cmp_or_mv $f1 $c1
       ;;
     (-v) 
-      f1=`mktemp`
-      cd ~/Downloads/video
-      find . -maxdepth  2 -print >$f1
-      cmp_or_mv $f1 $path1/video
+      f1=video
+      c1='find ~/Downloads/video -maxdepth  2 -print' 
+      cmp_or_mv $f1 $c1
       ;;
     (-b)
-      f1=`mktemp`
-      sha256sum `ls -1 -t /media/boot_ro/*.img`>$f1
-      cmp_or_mv $f1 $path1/init_sign
+      f1=init_sign
+      c1='sha256sum `ls -1 -t /media/boot_ro/*.img`'
+      cmp_or_mv $f1 $c1
       cmp -s ~/.config/priv2/test.kdb  /media/boot_ro/.mirror/test.kdb || echo "(WW) not same "
       ;;
     (-s)
-      f1=$path1/wz9hJi6.gz.gpg
+      f1=wz9hJi6.gz.gpg
       gpg -o - $f1 |zcat  | sed -n "$(expr $2 + 1),+1p"
        shift
       ;;
@@ -46,3 +54,4 @@ do
     shift
 done
 
+popd
