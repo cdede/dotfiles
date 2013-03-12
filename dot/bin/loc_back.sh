a116e69d29fd982643b61b5bae9b7fdac627ef7a
noname <samqhrobya@liquidid.net>
2013-03-12 10:48:49 +0900

    setfacl difp_oath

--- dot/bin/loc_back.sh
+++ dot/bin/loc_back.sh
@@ -9,7 +9,7 @@ back_files()
   $cmd1 -c 'ls -1 -t /media/boot_ro/*.img | xargs sha256sum' -f init_sign
 }
 pushd ~/.config/priv1
-set -- $(getopt s:lb "$@" )
+set -- $(getopt slb "$@" )
 while [ $# -gt 0 ]
 do
     case "$1" in
@@ -21,7 +21,8 @@ do
       ;;
     (-s)
       f1=wz9hJi6.gz.gpg
-      gpg -o - $f1 |zcat  | sed -n "$(expr $2 + 1),+1p"
+
+      gpg -o - $f1 |zcat  | sed -n "$(expr $(awk '{print $5}' /var/difp_oath) + 1),+1p"
        shift
       ;;
     (--) shift; break;;
